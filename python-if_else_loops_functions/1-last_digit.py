04503b110418746f03dc7eb8970fc7d8c48438f9
Khosuke <9814@holbertonstudents.com>
2025-01-13 14:41:45 +0100

    Fixed grammatical error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
 if number % modulo > 5:
     print(f"Last digit of {number} is {number % modulo} and is greater than 5")
 elif number % modulo == 0:
-    print(f"Last digif of {number} is {number % modulo} and is 0")
+    print(f"Last digit of {number} is {number % modulo} and is 0")
 else:
-    print(f"Last digif of {number} is {number % modulo}\
+    print(f"Last digit of {number} is {number % modulo}\
  and is less than 6 and not 0")
