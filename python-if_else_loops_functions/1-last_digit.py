39c93011c0b9d517b242b508da205b5a9bdffe5e
Khosuke <9814@holbertonstudents.com>
2025-01-13 14:38:22 +0100

    Edited files to comply to pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 modulo = 10
 if number < 0:
-	modulo = -10
+    modulo = -10
 if number % modulo > 5:
-	print(f"Last digit of {number} is {number % modulo} and is greater than 5")
+    print(f"Last digit of {number} is {number % modulo} and is greater than 5")
 elif number % modulo == 0:
-	print(f"Last digif of {number} is {number % modulo} and is 0")
+    print(f"Last digif of {number} is {number % modulo} and is 0")
 else:
-	print(f"Last digif of {number} is {number % modulo} and is less than 6 and not 0")
+    print(f"Last digif of {number} is {number % modulo}\
+ and is less than 6 and not 0")
