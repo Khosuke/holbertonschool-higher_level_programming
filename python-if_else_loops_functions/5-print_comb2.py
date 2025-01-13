c2560b3077729d1a74aec139907f9ecb0268f400
Khosuke <9814@holbertonstudents.com>
2025-01-13 15:44:30 +0100

    removed one loop and use only one variable

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print(f"{i}", end="")
-        print(f"{j}", end="")
-        print(", ", end="")
-print("")
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
+
