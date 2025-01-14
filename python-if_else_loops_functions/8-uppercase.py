cbe715d56ab1b7fddcb736347a22828cb78cfe45
Khosuke <9814@holbertonstudents.com>
2025-01-14 10:22:15 +0100

    changed value attributed to c to an integer and print it as a char

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,8 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
+            c = ord(c) - 32
+        else:
+            c = ord(c)
+        print('{:c}'.format(c), end='')
     print()
