a16f4460f873a0cfb30343b697c90ea205bdce95
Khosuke <9814@holbertonstudents.com>
2025-01-15 10:23:07 +0100

    changed string for 1 argument, and how the string if formated

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
     str = "arguments:"
-    j = 1
-    args = sys.argv[1:]
-    if len(args) == 1:
-        str = "argument"
-    if len(args) == 0:
+    nb_arg = len(argv) - 1
+
+    if nb_arg == 1:
+        str = "argument:"
+    if nb_arg == 0:
         str = "arguments."
-    print("{:d} {:s}".format(len(args), str))
-    if len(args) != 0:
-        for i in args:
-            print("{:d}: {:s}".format(j, i))
-            j += 1
+    print("{:d} {:s}".format(nb_arg, str))
+    if nb_arg > 0:
+        for i in range (1, nb_arg + 1):
+            print("{:d}: {:s}".format(i, argv[i]))
