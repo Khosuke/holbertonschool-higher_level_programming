fbae8388ff600e22a21d113d37dcabbeed4756d8
Khosuke <9814@holbertonstudents.com>
2025-01-15 16:41:14 +0100

    program that handles basic operations

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operators_list = ["+", "-", "*", "/"]    
+    a = int(argv[1]) 
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator not in operators_list:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            result = div(a, b)
+        print("{} {} {} = {}".format(a, operator, b, result))
