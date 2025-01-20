0124dbae214d9c30a8daaa4329b3145552f5bede
Khosuke <9814@holbertonstudents.com>
2025-01-20 16:05:45 +0100

    changed inital value of variable biggest_int from 0 to first element in my_list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return
-    biggest_int = 0
+    biggest_int = my_list[0]
     for i in my_list:
         if biggest_int < i:
             biggest_int = i
