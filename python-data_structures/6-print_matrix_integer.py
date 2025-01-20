bfa7f34dd8c62a144867e49e9fa37fdddd4d9927
Khosuke <9814@holbertonstudents.com>
2025-01-20 14:22:05 +0100

    changed string format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,10 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i]) - 1):
-            print("{} ".format(matrix[i][j]), end="")
+            print("{:d} ".format(matrix[i][j]), end="")
         try:
             j
         except j.DoesNotExist:
             print("")
         else:
-            print("{}".format(matrix[i][j]))
+            print("{:d}".format(matrix[i][j]))
