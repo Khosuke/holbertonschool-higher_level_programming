a07a3e71686c3768049b2eb6cd5ff97e26ad2ae2
Khosuke <9814@holbertonstudents.com>
2025-01-20 10:59:54 +0100

    string format to print integer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,5 +3,4 @@
 def print_list_integer(my_list=[]):
     len_list = len(my_list)
     for i in range(0, len_list):
-        str = "{}"
-        print(str.format(my_list[i]))
+        print("{:d}".format(my_list[i]))
