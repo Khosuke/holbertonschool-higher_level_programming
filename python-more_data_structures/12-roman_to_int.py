3e4e177bb2dc12f9ace0a52d186c77bc3c3d1d29
Khosuke <9814@holbertonstudents.com>
2025-01-22 13:20:28 +0100

    added condition if roman_string is not a string or is None

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) is not str:
+        return (0)
     rNum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     l_str = len(roman_string)
