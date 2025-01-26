6985e29a1a249a1494c4379012726cc5bfb63685
Khosuke <9814@holbertonstudents.com>
2025-01-26 17:46:58 +0100

    removed whitespaces

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     import sys
-    
-    try:    
+
+    try:
         result = fct(*args)
         return (result)
     except Exception as err:
