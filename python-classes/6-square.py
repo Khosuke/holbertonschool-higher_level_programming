ba00c396bbeb5190a8c96dc8c24189dd9865d630
Khosuke <9814@holbertonstudents.com>
2025-01-27 11:10:09 +0100

    Create class Square with position private attribute, and print the square according to the coordinates position

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Create a square class."""
+
+
+class Square:
+    """Define a square class."""
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for x in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            for y in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not (isinstance(elem, int) for elem in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
