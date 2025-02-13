f35309fc6ee3676f6f32bc7a70e7ddb4d4a85540
Khosuke <9814@holbertonstudents.com>
2025-02-13 22:23:02 +0100

    Square class fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,14 +3,13 @@
 This module defines a Square class that inherits
 from the Rectangle class.
 """
-RectangleClass = __import__("9-rectangle").Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
-class Square(RectangleClass):
+class Square(Rectangle):
     """
     A Square class that inherits from Rectangle class.
     """
-
     def __init__(self, size):
         """
         Initialize a new Square object.
@@ -18,8 +17,8 @@ class Square(RectangleClass):
             size: the size of the Square
         """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """
