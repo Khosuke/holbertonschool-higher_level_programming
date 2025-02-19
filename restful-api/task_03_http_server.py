2857359a13438e51e2e91e6b43dfc27be0d9ef90
Khosuke <9814@holbertonstudents.com>
2025-02-19 09:56:50 +0100

    Added documentation for the module, class and method

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+This modules defines a subclass of BaseHTTPRequestHandler
 """
 import http.server
 import socketserver
@@ -9,20 +9,27 @@ import json
 
 class http_SubClass(http.server.BaseHTTPRequestHandler):
     """
-    
+    This subclass implement a method to handle GET requests.
     """
     def do_GET(self):
+        """
+        This methods is used to handle GET requests when accessing
+        'data', 'info' and 'status' endpoint.
+        And returns a 404 Not Found status for undefined endpoint.
+        """
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
+
         elif self.path == "/data":
             dataset = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
+
         elif self.path == "/info":
             dataset = {"version": "1.0", "description":
                        "A simple API built with http.server"}
@@ -30,6 +37,7 @@ class http_SubClass(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
