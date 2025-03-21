b4b5e80cd18f292288a7ae7f5444cd64c69d2908
Khosuke <9814@holbertonstudents.com>
2025-03-21 20:54:57 +0100

    Fixed pycodestyle, added navigation bar to header template

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -4,8 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def landing_page():
+def home():
     return render_template('index.html')
 
 
@@ -17,6 +18,7 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
