e72330cc62a88b47f8e7a13916ff09262de8e2f2
Khosuke <9814@holbertonstudents.com>
2025-04-04 13:45:25 +0200

    Change click event listener to red_header

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
-header.addEventListener("click", () => {
+const red_header = document.getElementById("red_header");
+red_header.addEventListener("click", () => {
   header.style.color = "#FF0000";
 });
